b64a49902fb4c23b63e4e603ec524753755451da
LennyBTS <113920350+LennyBTS@users.noreply.github.com>
2022-09-21 09:07:34 +0200

    Update group.cpp

--- group.cpp
+++ group.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {    
     int number;
 
-    cout << "Enter an integer: ";
+    cout << "Enter an integer: " //teeeexxxxxtttttt
     cin >> number;
   
     cout << "You entered " << number;    
